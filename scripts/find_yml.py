73a7e00a314073c282397a86ade22bc3b0f7d76c
aegypius <innercircle@aegypius.com>
2015-11-17 14:04:29 +0100

    Allows to limit docker-compose search to a given depth (default: unlimited)

--- scripts/find_yml.py
+++ scripts/find_yml.py
@@ -5,13 +5,29 @@ find docker-compose.yml files
 import fnmatch
 import os
 
+MAX_DEPTH=os.environ.get('MAX_DEPTH', -1)
+
+def walklevel(some_dir, level=-1):
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if level != -1 and num_sep + level <= num_sep_this:
+            del dirs[:]
+
+
 def find_yml_files(path):
     """
     find docker-compose.yml files in path
     """
     matches = {}
-    for root, _, filenames in os.walk(path):
-        for _ in fnmatch.filter(filenames, 'docker-compose.yml'):
-            key = root.split('/')[-1]
-            matches[key] = os.path.join(os.getcwd(), root)
+    try:
+        for root, _, filenames in walklevel(path, MAX_DEPTH):
+            for dirs in fnmatch.filter(filenames, 'docker-compose.yml'):
+                key = root.split('/')[-1]
+                matches[key] = os.path.join(os.getcwd(), root)
+    except AssertionError, e:
+        pass
     return matches
